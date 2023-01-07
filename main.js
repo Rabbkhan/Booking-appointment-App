0027e5d52029d7718c78a5837fb89aeee894a2a4
Rabbil khan <rabbilkhan125@gmail.com>
2023-01-07 22:48:36 +0530

    adding object part

--- main.js
+++ main.js
@@ -4,7 +4,14 @@ let btn = document.querySelector('.btn');
 
 btn.addEventListener('click', e =>{
     e.preventDefault();
-
 localStorage.setItem('Name', Name.value);
 localStorage.setItem('Phone', Ph.value);
 })
+
+let details = {
+
+    Name : "Rabbil",
+    age: 56
+};
+let myobj_serialized = JSON.stringify(details);
+localStorage.setItem('myobj', myobj_serialized);
